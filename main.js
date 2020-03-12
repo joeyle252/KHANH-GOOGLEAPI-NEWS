c9f0983264080059f538302235c3c5ac74b02b92
lekimhongkhanh <lekimhongkhanh@gmail.com>
2020-03-12 21:10:28 +0700

    update select for general entertaiment helth and business

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 
 let newsList = [];
 let page = 1;
+let apiKey = 'fd0ab51d4e2b4838bf61e1a790dacdb7'
 let moreButton = document.querySelector(".moreButton");
+
+let shouldReplaceAllArticles = true;
+
 moreButton.addEventListener("click", () => {
+    shouldReplaceAllArticles = false;
     page = page + 1;
     callAPI();
 });
 
+
+
 let callAPI = async () => {
-    let apiKey = 'fd0ab51d4e2b4838bf61e1a790dacdb7'
+    if (page > 4) {
+        return;
+    }
+
     let url = `https://newsapi.org/v2/everything?page=${page}&pageSize=20&q=vietnam&apiKey=${apiKey}`
 
     let data = await fetch(url);
@@ -22,8 +32,43 @@ let callAPI = async () => {
     render(newsList);
 }
 
-const ArticleCount = (articles) => {
-    document.getElementById("numberOfArticles").innerHTML = `${articles.length} of ${newsArticles.length}`;
+let searchBySource = () => {
+    let sourceNames = newsList.map((item) => item.source.name)
+    console.log("source", source);
+    let sourceObJect = sourceNames.reduce((total, name) => {
+        if (name in total) {
+            total[name]++
+        } else {
+            total[name] = 1
+        }
+        return total;
+    }, {})
+
+    let sourceArray = Object.keys(sourceObject);
+
+    let htmlForSource = sourceArray.map((item) => `<input onchange='sourceClicked(${item}') type="checkbox" id="${item}"/> ${item} (${sourceObject[item]})`)
+    document.getElementById('sourceArea').innerHTML = htmlForSource;
+
+}
+
+let sourceClicked = index => {
+    if (document.getElementById(index).checked == true) {
+        let filteredNews = newsList.filter(item => item.source.name === index);
+        render(filteredNews);
+    } else {
+        render(newsList);
+    }
+};
+
+let searchByCategory = async () => {
+    shouldReplaceAllArticles = true;
+    let category = document.getElementById("category").value;
+    let url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${apiKey}`;
+    let data = await fetch(url);
+    let result = await data.json();
+
+    newsList = result.articles;
+    render(newsList);
 }
 
 let render = (array) => {
@@ -42,9 +87,14 @@ let render = (array) => {
          </div>
         `
     }).join('')
-    const newsDiv = document.createElement("div");
-    newsDiv.innerHTML = htmlForNews;
-    document.getElementById('newsArea').appendChild(newsDiv); 
+    if(shouldReplaceAllArticles === true){
+        document.getElementById('newsArea').innerHTML=htmlForNews; 
+    }else {
+        const newsDiv = document.createElement("div");
+        newsDiv.innerHTML = htmlForNews;
+        document.getElementById('newsArea').appendChild(newsDiv); 
+    }
+    
 };
 callAPI();
 
